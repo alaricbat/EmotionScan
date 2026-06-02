f2788ebe49bc61364bee735726ff52be0cb0ac00
alaricbat <tatchu2001@gmail.com>
2026-06-02 21:37:48 +0700

    Common:20260602:SVM:add more prediction function

--- app/src/main/java/com/advance/emotionscanapp/util/svm/SVMClassifier.kt
+++ app/src/main/java/com/advance/emotionscanapp/util/svm/SVMClassifier.kt
@@ -1,8 +1,7 @@
-package com.advance.emotionscanapp.util.json
+package com.advance.emotionscanapp.util.svm
 
 import android.content.Context
 import com.advance.emotionscanapp.util.log.Log
-import org.json.JSONArray
 import org.json.JSONObject
 import java.nio.charset.StandardCharsets
 
@@ -24,7 +23,7 @@ class SVMClassifier {
 
     private fun loadModelFromAssets(context: Context) {
         try {
-            val inputStream = context.assets.open("svm_model.json")
+            val inputStream = context.assets.open(SVMConstant.SVM_MODEL)
             val size = inputStream.available()
             val buffer = ByteArray(size)
             inputStream.read(buffer)
@@ -33,9 +32,9 @@ class SVMClassifier {
             val jsonString = String(buffer, StandardCharsets.UTF_8)
             val jsonObj = JSONObject(jsonString)
 
-            this.mBias = jsonObj.getDouble("bias").toFloat()
+            this.mBias = jsonObj.getDouble(SVMConstant.SVM_BIAS).toFloat()
 
-            val jsonWeights = jsonObj.getJSONArray("weights")
+            val jsonWeights = jsonObj.getJSONArray(SVMConstant.SVM_WEIGHTS)
             this.mWeights = FloatArray(jsonWeights.length())
             for (i in 0 until jsonWeights.length()) {
                 this.mWeights[i] = jsonWeights.getDouble(i).toFloat()
@@ -46,6 +45,26 @@ class SVMClassifier {
             Log.e(TAG, e.message)
             this.isMoreLoaded = false
         }
+    }
+
+    fun predict(input: FloatArray): String {
+
+        if (!isMoreLoaded) {
+            return "Model Error"
+        }
+
+        var score = mBias
+
+        for (i in 0 until input.size) {
+            score += mWeights[i] * input[i]
+        }
+
+        if (score >= 0.0f) {
+            return "Happy (+1)"
+        } else {
+            return "Sad (-1)"
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/advance/emotionscanapp/util/svm/SVMConstant.kt
@@ -0,0 +1,11 @@
+package com.advance.emotionscanapp.util.svm
+
+object SVMConstant {
+
+    const val SVM_BIAS = "bias"
+
+    const val SVM_WEIGHTS = "weights"
+
+    const val SVM_MODEL = "svm_model.json"
+
+}
